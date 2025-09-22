d7a1e8f74f6ab51b59e4843c9ab0555f834c02bf
Chris Abbato <chris.abbato@principles.com>
2025-09-22 13:50:03 -0500

    Add project files

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom";
 import { App } from "./components/App";
 import { QueryClient, QueryClientProvider } from "react-query";
 
-function renderRoot(): void {
+async function renderRoot(): Promise<void> {
   const rootElement = document.getElementById("root");
   if (!rootElement) {
     throw new Error('Root element with id "root" not found');
   }
+  const { worker } = await import("./mocks/browser");
+  await worker.start({ onUnhandledRequest: "bypass" });
   const queryClient = new QueryClient();
   ReactDOM.render(
     <QueryClientProvider client={queryClient}>
